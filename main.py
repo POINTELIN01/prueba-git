5f26486861a9061cdc7a3c3e58802e8c043a2962
José Alfredo Aguilar Pointelin <24014646@es.uveg.edu.mx>
2026-02-01 22:10:22 -0600

    Mejora validación de entrada

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 boleto = input("¿Tiene usted boleto? si/no ").lower()
 
-if boleto == "si":
+if boleto in ["si", "sí"]:
     print("Puede entrar")
-else:
+elif boleto == "no":
     print("No puede entrar")
-    
+else:
+    print("Respuesta no válida")
